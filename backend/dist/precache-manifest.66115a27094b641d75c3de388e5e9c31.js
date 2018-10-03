18abfc470279600fb184cb98f162efe88c5d8618
Liron Reshef Steinberger <reshef.liron@gmail.com>
2018-10-04 00:22:35 +0300

    updating section 1 , starting scket

---
+++ backend/dist/precache-manifest.66115a27094b641d75c3de388e5e9c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3ea14f6b088a01eb5735",
+    "url": "/js/chunk-vendors.ca9873f4.js"
+  },
+  {
+    "revision": "402bf87ee416466643cf",
+    "url": "/js/app.b5c1197c.js"
+  },
+  {
+    "revision": "06a9062cffc4ea03b93af1f204ea8aa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402bf87ee416466643cf",
+    "url": "/css/app.94f00a75.css"
+  }
+];
