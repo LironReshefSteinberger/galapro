53ff3fbf81f21879da5994ea5df3e80cbdf0a664
Liron Reshef Steinberger <reshef.liron@gmail.com>
2018-10-03 18:11:33 +0300

    first section -update

---
+++ backend/dist/precache-manifest.b3a24f2e0ca4b910aa34ca6f72679a97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ee786202039fb39f15bb",
+    "url": "/js/chunk-vendors.0b7e1b25.js"
+  },
+  {
+    "revision": "a953a7a3dfe1cfb8303c",
+    "url": "/js/app.53c2d19d.js"
+  },
+  {
+    "revision": "413dbcdc7a5c8cae0a711f0677757a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a953a7a3dfe1cfb8303c",
+    "url": "/css/app.94f00a75.css"
+  }
+];
